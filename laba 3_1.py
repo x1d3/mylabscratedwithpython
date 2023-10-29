8c18a243a7d005c2c2055d181edc1f31cb2930f4
Andrew <86807922+x1d3@users.noreply.github.com>
2023-10-29 15:29:40 +0300

    Add files via upload
    added more old lab work

---
+++ laba 3_1.py
@@ -0,0 +1,34 @@
+import math
+
+def input_positive_float(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value < 0:
+                print("Пожалуйста, введите положительное число.")
+            else:
+                return value
+        except ValueError:
+            print("Пожалуйста, введите числовое значение.")
+
+def input_coordinate(prompt):
+    while True:
+        value = input(prompt)
+        if value.replace(".", "").replace("-", "").isdigit():  
+            return float(value)
+        else:
+            print("Пожалуйста, введите числовое значение для координаты.")
+
+
+a1 = input_coordinate("Введите координату a1: ")
+a2 = input_coordinate("Введите координату a2: ")
+
+
+radius = input_positive_float("Введите радиус окружности: ")
+
+distance = math.sqrt(a1**2 + a2**2)
+
+if distance <= radius:
+    print(f"Точка A({a1}, {a2}) попадает в окружность с радиусом {radius}.")
+else:
+    print(f"Точка A({a1}, {a2}) не попадает в окружность с радиусом {radius}.")
