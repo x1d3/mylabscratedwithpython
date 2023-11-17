13d77080d733e014e0109c47df711540f0af5a6a
x13d <dimasik7557@gmail.com>
2023-11-18 01:45:23 +0300

    laba 5.3  added

---
+++ laba 5_1.py
@@ -0,0 +1,12 @@
+def print_age(name, years):                                         #Заголовок процедуры
+    print(name + ' is ' + str(years) + ' years old.')  #Тело процедуры
+
+friend = input('Name: ')
+age = int(input(' age: '))
+print_age(friend, age)                                                   #Вызов процедуры
+friend = input('Name: ')
+age = int(input(' age: '))
+print_age(friend, age)                                                    #Вызов процедуры
+friend = input('Name: ')
+age = int(input(' age: '))
+print_age(friend, age)
--- laba 5_3.py
+++ laba 5_3.py
@@ -1,22 +1,35 @@
-from scipy.integrate import quad
-import numpy as np
+import sympy as sp
 
-# Запрос пользовательского ввода
-n = int(input("Введите значение n (целое число): "))
-a = float(input("Введите нижнюю границу интегрирования a: "))
-b = float(input("Введите верхнюю границу интегрирования b: "))
-
-# Функция, которую нужно интегрировать
-def integrand(x):
+def integral_recursive(x, n, a, b):
     if n == 0:
         return x
     elif n == 1:
-        return np.log(np.tan(np.pi/4 + x/2))
+        return sp.log(sp.tan(sp.pi/4 + x/2))
     else:
-        return (1 / (n - 1)) * np.sin(x) / (np.cos(x)(n - 1) + (n - 2) / (n - 1) * (1 / np.cos(x)(n - 2)))
+        return -1/(n-1) * sp.sin(x)/sp.cos(x)**(n-1) + (n-2)/(n-1) * integral_recursive(x, n-2, a, b)
 
-# Вычисление определенного интеграла
-result, _ = quad(integrand, a, b)
+# Переменная интегрирования
+x = sp.symbols('x')
 
-# Вывод результата
-print("Результат вычисления определенного интеграла:", result)
+# Ввод пользователя с проверкой
+while True:
+    try:
+        n = int(input("Введите значение n (целое число >= 0): "))
+        if n < 0:
+            print("Пожалуйста, введите неотрицательное число.")
+        else:
+            break
+    except ValueError:
+        print("Пожалуйста, введите целое число.")
+
+# Заданные границы интегрирования
+a = 0
+b = sp.pi/2
+
+# Символьное вычисление интеграла
+integral_expr = integral_recursive(x, n, a, b)
+
+# Вычисление значения интеграла
+result = sp.integrate(integral_expr, (x, a, b))
+
+print(f"Значение интеграла: {result.evalf()}")
