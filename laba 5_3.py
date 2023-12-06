84d8309ec2dbfc57a20f597e8e082457d22620d3
x13d <dimasik7557@gmail.com>
2023-12-06 19:22:42 +0300

    laba 5 3 new

--- laba 5_3.py
+++ laba 5_3.py
@@ -1,35 +1,49 @@
-import sympy as sp
+import math
 
-def integral_recursive(x, n, a, b):
+def integral_value(x, n):
     if n == 0:
         return x
     elif n == 1:
-        return sp.log(sp.tan(sp.pi/4 + x/2))
+        if math.cos(math.pi/4 + x/2) == 0:
+            raise ValueError("Значение внутри тангенса вызывает деление на ноль. Измените ваши границы.")
+        return math.log(math.tan(math.pi/4 + x/2))
+    elif n > 1:
+        if math.cos(x)**(n-1) == 0:
+            raise ValueError("Значение внутри косинуса вызывает деление на ноль. Измените ваши границы.")
+        return recursive_integral_value(x, n)
     else:
-        return -1/(n-1) * sp.sin(x)/sp.cos(x)**(n-1) + (n-2)/(n-1) * integral_recursive(x, n-2, a, b)
+        raise ValueError("Введите значение n, равное нулю или больше.")
 
-# Переменная интегрирования
-x = sp.symbols('x')
+def recursive_integral_value(x, n):
+    if n == 0:
+        return x
+    elif n == 1:
+        return math.log(math.tan(math.pi/4 + x/2))
+    else:
+        if math.cos(x)**(n-1) == 0:
+            raise ValueError("Значение внутри косинуса вызывает деление на ноль. Измените ваши границы.")
+        first_equation = (1/(n-1)) * math.sin(x)/math.cos(x)**(n-1)
+        second_equation = (n-2)/(n-1) * recursive_integral_value(x, n-2)
+        return first_equation + second_equation
 
-# Ввод пользователя с проверкой
-while True:
+def main():
+    while True:
+        try:
+            n = int(input("Введите значение n: "))
+            if n < 0:
+                raise ValueError("Введите значение n, равное нулю или больше.")
+            a = float(input("Введите значение a: "))
+            b = float(input("Введите значение b: "))
+            break  # Прерываем цикл, если ввод корректен
+        except ValueError as e:
+            print(f"Ошибка ввода: {e}")
+
     try:
-        n = int(input("Введите значение n (целое число >= 0): "))
-        if n < 0:
-            print("Пожалуйста, введите неотрицательное число.")
-        else:
-            break
-    except ValueError:
-        print("Пожалуйста, введите целое число.")
+        result_a = integral_value(a, n)
+        result_b = integral_value(b, n)
+        print(f"Значение интеграла от {a} до {b} для n = {n} равно: {result_b - result_a}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
 
-# Заданные границы интегрирования
-a = 0
-b = sp.pi/2
-
-# Символьное вычисление интеграла
-integral_expr = integral_recursive(x, n, a, b)
-
-# Вычисление значения интеграла
-result = sp.integrate(integral_expr, (x, a, b))
-
-print(f"Значение интеграла: {result.evalf()}")
+if __name__ == "__main__":
+    main()
